635dad424d28ce00eb688c094bc4f42c5e6781ac
nathanharig <nathanharig@gmail.com>
2017-09-06 08:01:15 -0400

    Updated password

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Button, Card, CardItem, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: '' };
+  state = { email: '', password: '' };
 
   render() {
     return (
@@ -15,7 +15,15 @@ class LoginForm extends Component {
             onChangeText={email => this.setState({ email })}
           />
         </CardItem>
-        <CardItem />
+        <CardItem>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardItem>
 
         <CardItem>
           <Button>
